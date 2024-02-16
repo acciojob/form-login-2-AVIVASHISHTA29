7b604f7398c2472c42ff4a6df456306013a5a651
Avi Vashishta <avivashishta@gmail.com>
2024-02-16 15:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let form = document.getElementById("form1");
+form.addEventListener("submit",(e)=>
+	{
+	e.preventDefault();
+		let myStr = "";
+		for(let i=0;i<form.length;i++){
+			if(form.elements[i].value!=="Submit"){
+				myStr+= form.elements[i].name+": "+form.elements[i].value+"\n";
+			}
+		}
+		alert(myStr);
+	
+})
